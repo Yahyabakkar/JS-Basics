48822b0ade0018d672b9093eb60b0db8186946c4
yahyabakkar <yahyabakkar022@gmail.com>
2022-12-08 00:12:41 +0200

    step_6

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,6 @@
+function divise(){
+    var first_number= document.getElementById("first_number").value;
+    var second_number= document.getElementById("second_number").value;
+    var result=first_number % second_number;
+    alert ("remainder is :"+result)
+}
