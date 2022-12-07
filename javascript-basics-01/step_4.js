759b995c4694dc0ceebd65b2152ea409253f5d67
yahyabakkar <yahyabakkar022@gmail.com>
2022-12-07 16:28:14 +0200

    step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+function hello(){
+    var name =document.getElementById("name").value;
+    var surname =document.getElementById("surname").value;
+    var name =document.getElementById("city").value;
+    alert("Name:" + name + "\nsurname: " + surname+"\ncity: "+city);
+}
