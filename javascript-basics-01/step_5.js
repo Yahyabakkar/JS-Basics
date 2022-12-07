7327b89e2e95990f69235f23f966fbed9d28c0d0
yahyabakkar <yahyabakkar022@gmail.com>
2022-12-08 00:02:03 +0200

    step_5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var x= document.getElementById('first_number').value;
+    var y = document.getElementById('second_number').value;
+    var result = x * y;
+
+     alert(" Multiplication is: " + result);
+}   
